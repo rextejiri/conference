421881c973cd7e0bad3263d1b7927d51b1fdf89a
rextejiri <rextejiri@gmail.com>
2021-03-05 03:08:21 -0600

    search bar added

--- src/client/modules/my/sessionList/sessionList.js
+++ src/client/modules/my/sessionList/sessionList.js
@@ -1,10 +1,27 @@
 import { LightningElement } from 'lwc'
 import { getSessions } from 'data/sessionService'
+
+
+
+
 export default class SessionList extends LightningElement {
+    
+    
     sessions = [];
+
     connectedCallback(){
+
         getSessions().then(result => {
             this.sessions = this.allSessions = result;
+            // console.log(result, this.allSessions, this.sessions);
         });
+    }
+
+    handleSearchKeyInput(event) {
+        const searchKey = event.target.value.toLowerCase();
+        this.sessions = this.allSessions.filter(
+            session => session.name.toLowerCase().includes(searchKey)
+
+        )
     }
 }
